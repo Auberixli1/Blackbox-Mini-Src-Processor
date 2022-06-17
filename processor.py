b74bb92ad5edffe8235820eb6864fe3ca31654c2
Marcus Messer <marcus.messer@kcl.ac.uk>
2022-06-17 18:47:41 +0100

    PHD-275 Added logging

--- processor.py
+++ processor.py
@@ -1,3 +1,4 @@
+import logging
 import sys
 import os
 import xml.etree.ElementTree as ET
@@ -5,11 +6,20 @@ from pathlib import Path
 
 VERSION = 0.1
 
+logging.basicConfig(handlers=[
+        logging.FileHandler("debug.log"),
+        logging.StreamHandler()
+    ],
+    level=logging.ERROR,
+    format='%(asctime)s - %(message)s')
+
 
 def get_directory(root, org_dir, src_dir):
     """
     Used to transform the original directory to the new directory, and creates the directory if needed
     :param root: Root directory to convert
+    :param org_dir: The directory to convert from
+    :param src_dir: The directory to save to
     :return: New directory
     """
 
@@ -19,7 +29,6 @@ def get_directory(root, org_dir, src_dir):
         Path(new_root).mkdir(parents=True, exist_ok=True)
 
     return new_root
-
 
 def convert_file(root, file, new_root):
     """
@@ -36,13 +45,16 @@ def convert_file(root, file, new_root):
     new_file = file.replace(".xml", ".java")
 
     new_path = os.path.join(new_root, new_file)
+    logging.info("Converting: " + new_path)
+
     if not os.path.exists(new_path):
         try:
             with open(new_path, 'w') as f:
                 f.write(raw_src)
-        except EnvironmentError:
-            print("Unable to write to file, please check permissions and the log file for the exception.")
-            # TODO log excpetion
+        except EnvironmentError as e:
+            logging.fatal("ERROR SAVING FILE - Unable to write to file, "
+                          "please check permissions and the log file for the exception.")
+            logging.fatal(e)
             return True
 
 
@@ -53,9 +65,11 @@ def main(org_dir, src_dir):
     :param src_dir: The directory to save the Java files to.
     :return: None
     """
-    assert os.path.isdir(org_dir)
-    assert os.path.isdir(src_dir)
+    if not os.path.isdir(org_dir) or not os.path.isdir(src_dir):
+        logging.fatal("Either the origin directory or the destination directory are not valid")
+        return
 
+    count = 0
     for root, dirs, files in os.walk(org_dir):
         # For each file in partition convert and save
         new_root = get_directory(root, org_dir, src_dir)
@@ -65,10 +79,14 @@ def main(org_dir, src_dir):
                 is_error = convert_file(root, file, new_root)
 
                 if is_error:
-                    # End program
+                    logging.error("Critical error... Exiting")
                     return
-                
-                return
+
+        count += 1
+        if count == 3:
+            return
+
+    print("Complete, new files are in", args[1])
             
 
 if __name__ == '__main__':
@@ -76,15 +94,16 @@ if __name__ == '__main__':
     opts = [opt for opt in sys.argv[1:] if opt.startswith("-")]
     args = [arg for arg in sys.argv[1:] if not arg.startswith("-")]
 
+    if "-v" in opts or "--verbose" in opts:
+        logging.getLogger().setLevel(logging.INFO)
+    if "--version" in opts:
+        print(VERSION)
+
     if len(args) != 2:
         print("Please add the directory to convert and the directory you want to save to.")
         print("python3 processor.py /data/mini /data/minisrc")
         print("Use -v to enable logging")
+        logging.info("Not enough arguments to start process")
     else:
-        if "-v" or "--verbose" in opts:
-            print("VERBOSE")
-            pass
-        if "--version" in opts:
-            print(VERSION)
-
         main(org_dir=args[0], src_dir=args[1])
+
