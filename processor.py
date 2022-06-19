70975ab6a9f22113da556e4345506676e196e4d5
Marcus Messer <marcus.messer@kcl.ac.uk>
2022-06-19 16:25:36 +0100

    PHD-275 Refactored variable names

--- processor.py
+++ processor.py
@@ -1,7 +1,7 @@
 import logging
 import sys
 import os
-import xml.etree.ElementTree as ET
+import xml.etree.ElementTree as ElementTree
 from pathlib import Path
 
 VERSION = 0.1
@@ -14,16 +14,16 @@ logging.basicConfig(handlers=[
     format='%(asctime)s - %(message)s')
 
 
-def get_directory(root, org_dir, src_dir):
+def get_directory(root, input_dir, output_dir):
     """
     Used to transform the original directory to the new directory, and creates the directory if needed
     :param root: Root directory to convert
-    :param org_dir: The directory to convert from
-    :param src_dir: The directory to save to
+    :param input_dir: The directory to convert from
+    :param output_dir: The directory to save to
     :return: New directory
     """
 
-    new_root = root.replace(org_dir, src_dir)
+    new_root = root.replace(input_dir, output_dir)
 
     if not os.path.exists(new_root):
         Path(new_root).mkdir(parents=True, exist_ok=True)
@@ -39,10 +39,10 @@ def convert_file(root, file, new_root):
     :param new_root: The new root path to save to
     :return: None
     """
-    xml_tree = ET.parse(os.path.join(root, file)).getroot()
+    xml_tree = ElementTree.parse(os.path.join(root, file)).getroot()
     latest_version = xml_tree.findall('unit').pop()
 
-    raw_src = ET.tostring(latest_version, encoding='unicode', method='text')
+    raw_src = ElementTree.tostring(latest_version, encoding='unicode', method='text')
 
     new_file = file.replace(".xml", ".java")
 
@@ -60,22 +60,21 @@ def convert_file(root, file, new_root):
             return True
 
 
-def main(org_dir, src_dir):
+def main(input_dir, output_dir):
     """
     Main processor for converting SrcML to raw source.
-    :param org_dir: The original directory to convert
-    :param src_dir: The directory to save the Java files to.
+    :param input_dir: The original directory to convert
+    :param output_dir: The directory to save the Java files to.
     :return: None
     """
-    if not os.path.isdir(org_dir) or not os.path.isdir(src_dir):
+    if not os.path.isdir(input_dir) or not os.path.isdir(output_dir):
         logging.fatal("Either the origin directory or the destination directory are not valid")
         return
 
-    count = 0
-    for root, dirs, files in os.walk(org_dir):
+    for root, dirs, files in os.walk(input_dir):
         print(files)
         # For each file in partition convert and save
-        new_root = get_directory(root, org_dir, src_dir)
+        new_root = get_directory(root, input_dir, output_dir)
 
         for file in files:
             if file.endswith(".xml"):
@@ -103,5 +102,5 @@ if __name__ == '__main__':
         print("Use -v to enable logging")
         logging.info("Not enough arguments to start process")
     else:
-        main(org_dir=args[0], src_dir=args[1])
+        main(input_dir=args[0], output_dir=args[1])
 
